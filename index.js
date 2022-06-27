622a866a634536b3baa65d6411f0d3e8b7e646c4
Rrhul0 <rr025765@gmail.com>
2022-06-28 03:14:47 +0530

    first commit added api for creating user
    and store user information on mongodb cloud

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
+
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+dotenv.config()
+
+const uri = `mongodb+srv://MONGO14:${process.env.MONGO_DB_PASSWORD}@test.pty3p.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.post('/signup', async (req, res) => {
+    const data = req.body
+    console.log(data)
+    if (!data) {
+        res.statusCode(204)
+        res.send('No Data Provied')
+        return
+    }
+    try {
+        await client.connect()
+        const collection = client.db('cryptonaukri').collection('users')
+        collection.insertOne(data)
+
+        res.send('User Created')
+
+        await client.db('admin').command({ ping: 1 })
+    } finally {
+        await client.close()
+        return
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
